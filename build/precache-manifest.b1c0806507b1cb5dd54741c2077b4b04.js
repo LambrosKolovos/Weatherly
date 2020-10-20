35dbe2c385e72072b1843daff4631e5f9b19d2fd
LambrosKolovos <kolovosl@csd.auth.gr>
2020-10-20 18:53:20 +0300

    Build folder

---
+++ build/precache-manifest.b1c0806507b1cb5dd54741c2077b4b04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e312af563da3fb0b9a91a98be881bdde",
+    "url": "/Weatherly/index.html"
+  },
+  {
+    "revision": "cd4c899c496f258d24a0",
+    "url": "/Weatherly/static/css/main.635d31dc.chunk.css"
+  },
+  {
+    "revision": "950fa604c498381ac7fa",
+    "url": "/Weatherly/static/js/2.1d17f4f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weatherly/static/js/2.1d17f4f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4c899c496f258d24a0",
+    "url": "/Weatherly/static/js/main.cc973be8.chunk.js"
+  },
+  {
+    "revision": "0c4bf5d14a247eea15ef",
+    "url": "/Weatherly/static/js/runtime-main.6614e5c0.js"
+  },
+  {
+    "revision": "f31c382de6080c1513edd5bb4b2117c1",
+    "url": "/Weatherly/static/media/weatherly-background.f31c382d.jpg"
+  }
+]);
